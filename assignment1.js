aea561d202f965ef3bcfa6e0aa4ea599aefad02c
Lingxiu Ll <163415163+Lingxiu99@users.noreply.github.com>
2024-06-28 12:40:48 +0200

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,51 @@
+// Task 1: Variable declarations and initializations
+let age = 27;
+let name = "Lingxiu LI"; 
+let isStudent = true;
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isStudent);
+
+// Task 2: Arithmetic Operations
+let a = 10;
+let b = 20;
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+console.log("Addition Result:", addition); 
+console.log("Subtraction Result:", subtraction); 
+console.log("Multiplication Result:", multiplication); 
+console.log("Division Result:", division);
+
+// Task 3: Comparison Operators
+let x = 5;
+let y = '10';
+
+console.log(x == y);
+console.log(x === y); 
+console.log(x != y); 
+console.log(x !== y);
+console.log(x > y); 
+console.log(x < y); 
+console.log(x >= y);
+console.log(x <= y);
+
+// Task 4: Type Coercion
+let numberString = "10";
+let actualNumber = 5;
+
+let Addition = numberString + actualNumber; 
+let Subtraction= numberString - actualNumber; 
+let Multiplication = numberString * actualNumber;
+let Division = numberString / actualNumber;
+
+console.log("Addition Result:", Addition);
+console.log("Subtraction Result:", Subtraction);
+console.log("Multiplication Result:", Multiplication);
+console.log("Division Result:", Division);
+
+
